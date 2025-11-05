ce6501f2bc863a0ea34d0e0e725e6ac8a5df850d
Afreen Mohammed <afreenaero@gmail.com>
2025-11-05 15:08:19 +0100

    update

--- Shapes.py
+++ Shapes.py
@@ -19,4 +19,12 @@ class Shape:
     def __repr__(self):  # use it for how code will look like
         return f"shape({self._x}, {self._y})"
     
-    def translate (self, dx, dy):  # 
+    def translate (self, dx, dy):  # to see dx and dy are numbers
+        if not isinstance(dx, (int, float)):   # if dx is not a number, then will stop and give erroe.
+            raise TypeError("dx must be a number")
+        
+        elif not isinstance(dy, (int, float)):    # if dy is not a number, then will give error
+            raise TypeError("dy must be a number")
+        
+        self._x += dx   # update x & y with dx and dy
+        self._y += dy
