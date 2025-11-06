548ce6790fda8e6b0647013d1bc6f961b9e72124
Afreen Mohammed <afreenaero@gmail.com>
2025-11-06 16:34:38 +0100

    updated

--- Circle.py
+++ Circle.py
@@ -1,5 +1,7 @@
 from shapes import Shape   # here importing the shape class from shape.py
-from math import pi       
+from math import pi   
+import matplotlib.pyplot as plt 
+from matplotlib.patches import circle as CirclePatch
 
 class Circle(Shape):        # here inherits from Shape class
     def __init__(self, x=0, y=0, radius=1):
@@ -10,19 +12,21 @@ class Circle(Shape):        # here inherits from Shape class
             raise ValueError("radius must be positive")
         self.radius = radius    
 
+    # read-only area property
     @property
     def area(self):
         return pi * self.radius ** 2
     
+    # read-only perimeter property
     @property
     def perimeter(self):
         return 2 * pi * self.radius
     
     def is_unit_circle(self):
-        # check if circle has radius = 1 and centered at origin
         return self.radius == 1 and self.x == 0 and self.y == 0
     
-    def __eq__(self, other):
+    #  equality check
+    def __eq__(self, other):  
         return isinstance(other, Circle) and self.radius == other.radius
     
 
@@ -38,3 +42,9 @@ def __gt__(self, other):
 
 def __ge__(self, other):
     return self.area >= other.area
+
+def draw(self, ax):
+    # Drawing the circle using matplotlib.
+    circle_patch = CirclePatch((self.x, self.y), self.radius, fill=False, edgecolor = 'sky blue')
+    ax.add_patch(circle_patch)
+    ax.set_aspect('equal', adjustable = 'box')
