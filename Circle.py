f58e9589eb8f94c12441c07acab53847c00c783e
Afreen Mohammed <afreenaero@gmail.com>
2025-11-21 09:34:42 +0100

    updated

--- Circle.py
+++ Circle.py
@@ -7,7 +7,7 @@ from typing import Any
 
 
 class Circle(Shape):        #  Inherits from Shape class
-     def __init__(self, x, y, radius):
+    def __init__(self, x, y, radius):
         self._x = x
         self._y = y
         self._radius = radius 
@@ -22,14 +22,14 @@ class Circle(Shape):        #  Inherits from Shape class
 #         self._radius = float(radius)
 
         # radius read-only property
-        @property 
-        def radius(self) -> float:
-            return self._radius    
+    @property 
+    def radius(self) -> float:
+        return self._radius    
 
         # read-only area property
-        @property
-        def area(self) -> float:
-          return math.pi * (self._radius ** 2)  
+    @property
+    def area(self) -> float:
+        return math.pi * (self._radius ** 2)  
     
     # read-only perimeter property
     @property
@@ -56,25 +56,25 @@ class Circle(Shape):        #  Inherits from Shape class
             self._check_other_for_compare(other)
             return self.area < other.area
         
-        def __le__(self, other: Any) -> bool:
-            self._check_other_for_compare(other)
-            return self.area <= other.area
+    def __le__(self, other: Any) -> bool:
+        self._check_other_for_compare(other)
+        return self.area <= other.area
         
-        def __gt__(self, other: Any) -> bool:
-            self._check_other_for_compare(other)
-            return self.area > other.area
+    def __gt__(self, other: Any) -> bool:
+        self._check_other_for_compare(other)
+        return self.area > other.area
         
-        def __ge__(self, other: Any) -> bool:
-            self._check_other_for_compare(other)
-            return self.area >= other.area
+    def __ge__(self, other: Any) -> bool:
+        self._check_other_for_compare(other)
+        return self.area >= other.area
         
-        def is_unit_circle(self) -> bool:
-            # return Trure if radius == 1 (allowing small float error)
-            return math.isclose(self.radius, 1.0)
+    def is_unit_circle(self) -> bool:
+        # return Trure if radius == 1 (allowing small float error)
+        return math.isclose(self.radius, 1.0)
     
-        # __repr__and__str__for helpful text
-        def __repr__(self) -> str:
-            return f"Circle(x={self.x}, y={self.y}, radius={self.radius})"
+    # __repr__and__str__for helpful text
+    def __repr__(self) -> str:
+        return f"Circle(x={self.x}, y={self.y}, radius={self.radius})"
     
-        # def __str__(self) -> str:
-        #     return f"Circle with center ({self.x}, {self.y}) and radius {self.radius}"
+    # def __str__(self) -> str:
+        #return f"Circle with center ({self.x}, {self.y}) and radius {self.radius}"
